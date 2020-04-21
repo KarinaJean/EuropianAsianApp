e8c9e9202c60feea2a5ca0757fc3cad811329fbe
Karina <krebise@yahoo.com>
2020-04-21 18:15:21 +0200

    Initial Commit

---
+++ EuropianAsianApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using EuropianAsianApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EuropianAsianApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
